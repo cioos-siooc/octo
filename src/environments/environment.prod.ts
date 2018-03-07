8aca7ee8a5a3327910cb3118654e203497bfb85b
desjardinsa <desjardinsa@ogsl.ca>
2018-03-07 10:05:14 -0500

    Add new background layer + styling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mapapiUrl: 'https://ogsl.ca/mapapi/api',
-  backgroundLayerIds: ['2537']
+  backgroundLayerIds: ['2537', '2538', '2539']
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mapapiUrl: 'https://test-www.ogsl.ca/mapapi/api',
-  backgroundLayerIds: ['2537']
+  backgroundLayerIds: ['2537', '2538', '2539']
 };
