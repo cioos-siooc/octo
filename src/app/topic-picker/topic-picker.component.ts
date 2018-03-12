c1f907776858cfca577081ac8037f50b677cd30a
Brad Covey <bradcovey@gmail.com>
2018-03-12 09:56:38 -0400

    Sets the topic group to emodnet

--- src/app/topic-picker/topic-picker.component.ts
+++ src/app/topic-picker/topic-picker.component.ts
@@ -19,7 +19,7 @@ export class TopicPickerComponent implements OnInit {
 
   ngOnInit() {
     this.catalogState = this.store.select('catalog');
-    this.store.dispatch(new catalogActions.FetchTopicGroup(12));
+    this.store.dispatch(new catalogActions.FetchTopicGroup(5));
   }
 
   onClickTopic(id: number, topic: Topic) {
