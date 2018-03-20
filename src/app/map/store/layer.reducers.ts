295b9f65e9c1c05bfcfea6acd62e9d4a06d4e4c2
desjardinsa <desjardinsa@ogsl.ca>
2018-03-20 08:57:40 -0400

    Use properly immutable reducer for layer + add update layer action

--- src/app/map/store/layer.reducers.ts
+++ src/app/map/store/layer.reducers.ts
@@ -1,5 +1,6 @@
 import {Layer} from '../../shared/layer.model';
 import * as layerActions from './layer.actions';
+import {cloneDeep} from 'lodash';
 
 export interface State {
   layers: Layer[];
@@ -12,18 +13,22 @@ const initialState: State = {
 export function layerReducer(state = initialState, action: layerActions.LayerActions): State {
   switch (action.type) {
     case layerActions.ADD_LAYER:
-      return {
-        ...state,
-        layers: [...state.layers, action.payload]
-      };
+      const clonedState = cloneDeep(state);
+      clonedState.layers.push(action.payload);
+      return clonedState;
     case layerActions.DELETE_LAYER:
-      const newLayers = [...state.layers].filter((l: Layer) => {
+      const cloneState = cloneDeep(state);
+      cloneState.layers = cloneState.layers.filter((l: Layer) => {
         return l.uniqueId !== action.payload;
       });
-      return {
-        ...state,
-        layers: newLayers
-      };
+      return cloneState;
+    case layerActions.UPDATE_LAYER:
+      const clownState = cloneDeep(state);
+      const layerIndex = clownState.layers.findIndex((l) => l.uniqueId === action.payload.uniqueId);
+      if (layerIndex > -1) {
+        clownState.layers[layerIndex] = action.payload;
+      }
+      return clownState;
     default:
       return state;
   }
