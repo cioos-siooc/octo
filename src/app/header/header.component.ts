445752cb4e5a4dba98f9f788b37fc3cf69525e13
desjardinsa <desjardinsa@ogsl.ca>
2018-03-23 09:24:11 -0400

    Conceive first basic prototype to change language

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {environment} from '../../environments/environment';
+import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [NgbDropdownConfig]
 })
 export class HeaderComponent implements OnInit {
   titleKey: string;
 
-  constructor(private translateService: TranslateService) {
+  constructor(private translateService: TranslateService, private config: NgbDropdownConfig) {
+    config.placement = 'bottom-right';
+  }
+
+  onChangeLanguageClick() {
+    if (this.translateService.currentLang === 'fr') {
+      window.location.href = document.location.href.replace(environment.frenchLanguageIdentifier, environment.englishLanguageIdentifier);
+    } else if (this.translateService.currentLang === 'en') {
+      window.location.href = document.location.href.replace(environment.englishLanguageIdentifier, environment.frenchLanguageIdentifier);
+    }
   }
 
   ngOnInit() {
