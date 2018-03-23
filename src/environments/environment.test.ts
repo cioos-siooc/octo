8660da6c215f99885fe41a17e6d2c6aa66b96fc0
desjardinsa <desjardinsa@ogsl.ca>
2018-03-23 09:47:12 -0400

    Change test config to use mapapi2

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  mapapiUrl: 'https://test-www.ogsl.ca/mapapi/api',
+  mapapiUrl: 'http://srogslwebt01.uqar.ca:8080/mapapi2/api',
   backgroundLayerIds: ['2537', '2538', '2539'],
   topicGroupCode: 'ca.ogsl.infosl',
   titleKey: 'infosl-title',
