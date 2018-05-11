0fe29fdb9f343f0a377b0f71f13c4ba7c5525c25
jemag <bd.jemag@gmail.com>
2018-05-11 16:01:39 -0400

    Standardize layer information actions

--- src/app/layer-information/store/layer-information.reducers.ts
+++ src/app/layer-information/store/layer-information.reducers.ts
@@ -1,23 +1,23 @@
-import * as layerInformationActions from './layer-information.actions';
+import {LayerInformationActionsUnion, LayerInformationActionTypes} from './layer-information.actions';
 
 export interface State {
   informationHtml: string;
   selectedLayerId: number;
 }
 
-const initialState: State = {
+export const initialState: State = {
   informationHtml: null,
   selectedLayerId: null
 };
 
-export function layerInformationReducer(state = initialState, action: layerInformationActions.LayerInformationActions): State {
+export function layerInformationReducer(state = initialState, action: LayerInformationActionsUnion): State {
   switch (action.type) {
-    case layerInformationActions.SET_LAYER_INFORMATION:
+    case LayerInformationActionTypes.SET_LAYER_INFORMATION:
       return {
         ...state,
         informationHtml: action.payload
       };
-    case layerInformationActions.SET_SELECTED_LAYER_ID:
+    case LayerInformationActionTypes.SET_SELECTED_LAYER_ID:
       return {
         ...state,
         selectedLayerId: action.payload
