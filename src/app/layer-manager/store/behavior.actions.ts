172b578985bd911466154d99f421e0d5bf564351
jemag <bd.jemag@gmail.com>
2018-05-11 14:47:50 -0400

    Standardize behavior actions

--- src/app/layer-manager/store/behavior.actions.ts
+++ src/app/layer-manager/store/behavior.actions.ts
@@ -1,31 +1,33 @@
 import {Action} from '@ngrx/store';
 
-export const ADD_BEHAVIOR = 'BEHAVIOR_ADD';
-export const UPDATE_BEHAVIOR = 'BEHAVIOR_UPDATE';
-export const DELETE_BEHAVIOR = 'BEHAVIOR_DELETE';
+export enum BehaviorActionTypes {
+  ADD_BEHAVIOR = '[Behavior] Add',
+  UPDATE_BEHAVIOR = '[Behavior] Update',
+  DELETE_BEHAVIOR = '[Behavior] Delete',
+}
 
 export class AddBehavior implements Action {
-  readonly type = ADD_BEHAVIOR;
+  readonly type = BehaviorActionTypes.ADD_BEHAVIOR;
 
   constructor(public payload: any) {
   }
 }
 
 export class UpdateBehavior implements Action {
-  readonly type = UPDATE_BEHAVIOR;
+  readonly type = BehaviorActionTypes.UPDATE_BEHAVIOR;
 
   constructor(public payload: any) {
   }
 }
 
 export class DeleteBehavior implements Action {
-  readonly type = DELETE_BEHAVIOR;
+  readonly type = BehaviorActionTypes.DELETE_BEHAVIOR;
 
   constructor(public payload: string) {
   }
 }
 
-export type BehaviorActions =
+export type BehaviorActionsUnion =
   AddBehavior |
   UpdateBehavior |
   DeleteBehavior;
