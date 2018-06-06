02d20487cb8bf213e9640b8a82e8dd64e2a9fcc1
jemag <bd.jemag@gmail.com>
2018-06-06 11:51:39 -0400

    Add barrels for utils and high level one for store

---
+++ src/app/map/utils/click-formatter/index.ts
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export * from './field';
+export * from './click-formatter.util';
+export * from './click-formatter-factory.util';
+export * from './field-click-formatter.util';
+export * from './wms-text-click-formatter.util';
+
