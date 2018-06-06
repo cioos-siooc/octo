04596d452ca54a2718f2598b81f53c4ea0d56f1a
jemag <bd.jemag@gmail.com>
2018-06-06 11:37:02 -0400

    Add barrels for map store and services

---
+++ src/app/map/store/effects/index.ts
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export * from './base-layer.effects';
+export * from './catalog.effects';
+export * from './layer.effects';
+export * from './layer-information.effects';
+export * from './layer-presentation.effects';
+export * from './map-click.effects';
---
+++ src/app/map/store/reducers/index.ts
@@ -0,0 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export * from './base-layer.reducers';
+export * from './behavior.reducers';
+export * from './catalog.reducers';
+export * from './layer.reducers';
+export * from './map.reducers';
+export * from './map-click.reducers';
+export * from './popup.reducers';
