f0a6d60ec402946624f16cf7fc962a7f9889d7b3
jemag <bd.jemag@gmail.com>
2018-06-11 10:52:04 -0400

    Change configs to deploy maponion on test infrastructure

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   mapapiUrl: 'https://test-www.ogsl.ca/mapapi2/api',
   backgroundLayerCodes: ['bing.aerial', 'bing.road', 'bing.aerialWithLabels'],
-  topicGroupCode: 'ca.ogsl.infosl',
+  topicGroupCode: 'ca.ogsl.multi_topics',
   titleKey: 'infosl-title',
-  isTopicPickerActive: false,
+  isTopicPickerActive: true,
   defaultTopic: 'ca.ogsl.infosl',
   frenchLanguageIdentifier: 'ogsl',
   englishLanguageIdentifier: 'slgo'
