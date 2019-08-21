dfcc39273b3140b59eaa5663396c61ef36b0ec07
Hugo Lemieux <lemieuxh@ogsl.com>
2019-08-21 17:01:10 -0400

    New component for display and manage layer opacity

---
+++ src/app/map/components/layer-opacity/layer-opacity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layer-opacity',
+  templateUrl: './layer-opacity.component.html',
+  styleUrls: ['./layer-opacity.component.css']
+})
+export class LayerOpacityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
