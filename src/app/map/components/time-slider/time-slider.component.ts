174679215780a67e9dcd8a91d0cb4b4b09b76d09
Brad Covey <bradcovey@gmail.com>
2019-08-29 16:30:37 -0400

    Adds interactive functionality to time slider

--- src/app/map/components/time-slider/time-slider.component.ts
+++ src/app/map/components/time-slider/time-slider.component.ts
@@ -1,7 +1,6 @@
 import { DateRange } from '@app/shared/models/date-range.model';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { select, ScaleTime, scaleTime, axisBottom, timeFormat } from 'd3';
-import { container } from '@angular/core/src/render3';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, HostListener } from "@angular/core";
+import { select, scaleTime, axisBottom } from 'd3';
 
 @Component({
   selector: 'app-time-slider',
@@ -10,21 +9,28 @@ import { container } from '@angular/core/src/render3';
   encapsulation: ViewEncapsulation.None
 })
 export class TimeSliderComponent implements OnInit {
+  @ViewChild('nib') nib: ElementRef;
+
   containerSelector: string;
-  nibSelector: string;
   dateRange: DateRange;
   svg: any;
   scale: any;
   axis: any;
 
+  dragActive: boolean;
+  start: number;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.start = 0;
     this.containerSelector = '#slider';
     this.dateRange = {
       'start': new Date(2018, 1, 1),
       'end': new Date(2019, 1, 1)
     };
+    this.dragActive = false;
     this.initTimeSlider(this.containerSelector, this.dateRange);
   }
 
@@ -54,4 +60,36 @@ export class TimeSliderComponent implements OnInit {
       .call(this.axis);
   }
 
+  activateNibDrag() {
+    this.dragActive = true;
+    this.start = this.nib.nativeElement.parentElement.getBoundingClientRect().x;
+  }
+
+  @HostListener( 'document:mouseup' )
+  deactivateNibDrag(e) {
+    if ( this.dragActive) {
+      this.dragActive = false;
+      this.onmousemove(e);
+    }
+  }
+
+  @HostListener( 'document:mousemove', ['$event'] )
+  onmousemove(e) {
+    if ( this.dragActive ) {
+      let end = 0;
+      if ( e.pageX ) {
+        end = e.pageX;
+      } else if ( e.clientX ) {
+        end = e.clientX;
+      }
+      let diff = end - this.start;
+      const parentWidth = this.nib.nativeElement.parentElement.getBoundingClientRect().width;
+      if (diff < 0) {
+        diff = 0;
+      } else if (diff > parentWidth) {
+        diff = parentWidth;
+      }
+      this.nib.nativeElement.style.left = diff + 'px';
+    }
+  }
 }
