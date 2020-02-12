4ade1ded7ff02f29e621d2be524bdf8147e45aec
Brad Covey <bradcovey@gmail.com>
2020-02-12 10:01:51 -0500

    Adds action doc for the layer-information reducer

--- src/app/map/store/actions/layer-information.actions.ts
+++ src/app/map/store/actions/layer-information.actions.ts
@@ -11,16 +11,42 @@ export enum LayerInformationActionTypes {
   SET_SELECTED_LAYER_ID = '[Layer information] Set selected layer id',
 }
 
+/**
+ * Creates an instance of SetLayerInformation which can be dispatched to the store
+ *  SetLayerInformation sets the informationHtml property in the layer-information reducer
+ *
+ * @export
+ * @class SetLayerInformation
+ * @implements {Action}
+ */
 export class SetLayerInformation implements Action {
   readonly type = LayerInformationActionTypes.SET_LAYER_INFORMATION;
 
+  /**
+   *Creates an instance of SetLayerInformation.
+   * @param {string} payload - A string containing the HTML to be set
+   * @memberof SetLayerInformation
+   */
   constructor(public payload: string) {
   }
 }
 
+/**
+ * Creates an instance of SetSelectedLayerId which can be dispatched to the store
+ *  SetSelectedLayerId sets the selectedLayerId property in the layer-information reducer
+ *
+ * @export
+ * @class SetSelectedLayerId
+ * @implements {Action}
+ */
 export class SetSelectedLayerId implements Action {
   readonly type = LayerInformationActionTypes.SET_SELECTED_LAYER_ID;
 
+  /**
+   *Creates an instance of SetSelectedLayerId.
+   * @param {number} payload - The layer id to be set
+   * @memberof SetSelectedLayerId
+   */
   constructor(public payload: number) {
   }
 }
