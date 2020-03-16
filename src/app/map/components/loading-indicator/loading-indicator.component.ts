bee3e53b0cb573bf24f26418578539db3e8c9a9d
Hugo Lemieux <you@example.com>
2020-03-16 15:04:09 -0400

    Adds MPL to new files

--- src/app/map/components/loading-indicator/loading-indicator.component.ts
+++ src/app/map/components/loading-indicator/loading-indicator.component.ts
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { LoadingService } from './../../services/loading.service';
 import { Component, OnInit, OnDestroy, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { Subscription } from 'rxjs';
